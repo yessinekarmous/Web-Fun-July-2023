951439bb03c21b3c3dbd8bed3df15ab7689ac293
unknown <yessinekarmous25@gmail.com>
2023-07-25 16:36:34 +0100

    Liking likes part1 and part2

---
+++ Week 2/Day 5/Core/Liking Likes/part2/script.js
@@ -0,0 +1,8 @@
+var nbr0=[  document.querySelector("#nbr"),
+            document.querySelector("#nbr1"),
+            document.querySelector("#nbr2")]
+
+function addition(id){
+    nbr0[id].innerHTML++;
+    
+}
